d01a1912493a4991d2997c5605016b1d2fe91b5b
Jack Tiong <tiongyingkiat@gmail.com>
2021-05-20 18:32:55 +0800

    - Added APIs

--- constants/constants.js
+++ constants/constants.js
@@ -9,6 +9,13 @@ const marketTabs = [
     },
 ]
 
+//API
+// My Holdings
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${orderBy}&per_page=${perPage}&page=${page}&sparkline=${sparkline}&price_change_percentage=${priceChangePerc}&ids=${ids}
+
+// Coin Market
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${orderBy}&per_page=${perPage}&page=${page}&sparkline=${sparkline}&price_change_percentage=${priceChangePerc}
+
 const constants = {
     marketTabs
 };
